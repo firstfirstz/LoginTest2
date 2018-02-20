e6714ce080b0cbaa956050836cd8d560c68c56bd
firstfirstz <first.stg@gamil.com>
2018-02-20 15:03:15 +0700

    deja vu

--- app/src/main/java/sdu/cs58/pichsinee/logintest/LoginActivity.java
+++ app/src/main/java/sdu/cs58/pichsinee/logintest/LoginActivity.java
@@ -33,6 +33,11 @@ public class LoginActivity extends AppCompatActivity {
                 nameString = nameEditText.getText().toString().trim();
                 userString = usernameEditText.getText().toString().trim();
                 passString = passwordEditText.getText().toString().trim();
+
+                if ((nameString.length()==0)||(userString.length()==0)||(passString.length()==0)) {
+                    Toast.makeText(getApplicationContext(),"กรุณาใส่ข้อมูลให้ครบทุกช่อง",Toast.LENGTH_LONG).show();
+                }
+
                 // ตรวจสอบการ login
                 if ((userString.equals("admin")) && (passString.equals("1234"))) {
                     Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
